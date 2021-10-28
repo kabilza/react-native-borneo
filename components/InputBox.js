5808ec0384d1bf9f5bf7a82150c74b192914c912
kabilza <naruoluvohina@gmail.com>
2021-10-28 11:53:39 +0700

    added InputBox component (still)

---
+++ components/InputBox.js
@@ -0,0 +1,76 @@
+import React, { useReducer, useEffect } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+const INPUT_CHANGE = "INPUT_CHANGE";
+const INPUT_BLUR = "INPUT_BLUR";
+
+const InputBox = (props) => {
+  const initialState = {
+    value: props.initialValue ? props.initialValue : "",
+    isValid: props.initiallyValid,
+    touched: false,
+  };
+
+  const inputReducer = (action, state) => {
+    switch (action.type) {
+      case INPUT_CHANGE:
+        return "";
+      case INPUT_BLUR:
+        return "";
+        return state;
+    }
+  };
+
+  const [inputState, dispatch] = useReducer(inputReducer, initialState);
+
+  const textChangeHandler = (text) => {
+    let isValid = true;
+    dispatch({ type: INPUT_CHANGE, value: text, isValid: isValid });
+  };
+
+  const lostFocusHandler = () => {
+    dispatch({ type: INPUT_BLUR });
+  };
+
+  return (
+    <View style={styles.formControl}>
+      <Text style={styles.label}>{props.label}</Text>
+      <TextInput
+        {...props}
+        style={styles.input}
+        value={inputState.value}
+        onChangeText={textChangeHandler}
+        onBlur={lostFocusHandler}
+      />
+      {!inputState.isValid && inputState.touched && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{props.errorText}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formControl: {
+    width: "100%",
+  },
+  label: {
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  errorContainer: {
+    marginVertical: 5,
+  },
+  errorText: {
+    color: "red",
+    fontSize: 13,
+  },
+});
+
+export default InputBox;
